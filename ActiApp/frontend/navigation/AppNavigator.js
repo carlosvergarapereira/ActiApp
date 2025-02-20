572fc7dcb27a651cfaa9308d2ef38e8990783827
Carlos V. <charlie09042013@gmail.com>
2025-02-20 01:20:14 -0300

    modificaciones en las vistas y rutas

---
+++ ActiApp/frontend/navigation/AppNavigator.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LoginScreen from '../screens/LoginScreen';
+import HomeScreen from '../screens/HomeScreen';
+// ... importa las demás pantallas
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* ... añade las demás pantallas */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
